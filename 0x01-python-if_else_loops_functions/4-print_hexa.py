5aae6ace044c7d73835d638da3c138f32f2bf4aa
M-Steve1 <modebestephen2@gmail.com>
2022-11-29 13:40:00 -0800

    Fix bug on 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-j = 0
 for i in range(99):
-    if i > 9 and i <= 15:
-        print("{} = 0x{}".format(i, "abcdef"[j]))
-        j += 1
-    else:
-        print("{} = 0x{}".format(i, i))
+    print("{} = {}".format(i, hex(i)))
