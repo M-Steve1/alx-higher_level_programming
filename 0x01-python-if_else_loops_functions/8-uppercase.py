a6e2ae2f90acfc236f721f3e4eea603e2116d67e
M-Steve1 <modebestephen2@gmail.com>
2022-11-29 16:21:48 -0800

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lower = "abcdefghijklmnopqrstuvwxyz"
 def uppercase(str):
     for i in str:
-        if i in upper or i in lower:
-            print("{}".format(i).capitalize(), end="")
-        else:
-              print("{}".format(i), end="")
+        print("{}".format(i).capitalize(), end="")
     print()
